7a415b8fa4613ff351a90642029e36e3146d6f29
rinku-kr <rk24736599@gmail.com>
2024-01-26 22:09:14 +0530

    Adding multer middleware in register route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,16 @@
 import express from "express";
 import registerUser from "../controllers/userController.js";
+import upload from "../middlewares/multerMiddleware.js";
 
 const router = express.Router();
 
 // router.post("/register", registerUser);
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
 export default router;
