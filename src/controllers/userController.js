64c0e751ee849b96454e8cd984be231a1c05cffc
rinku-kr <rk24736599@gmail.com>
2024-01-28 00:36:44 +0530

    Implement aggregation pipeline

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -322,6 +322,78 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         .status(200)
         .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
+
+// Get user channel profile
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase(),
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers",
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo",
+            },
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers",
+                },
+                channelsSubscribedCount: {
+                    $size: "$subscribedTo",
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false,
+                    },
+                },
+            },
+        },
+
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                email: 1,
+                avatar: 1,
+                coverImage: 1,
+                subscribersCount: 1,
+                channelsSubscribedCount: 1,
+                isSubscribed: 1,
+            },
+        },
+    ]);
+
+    if (!channel.length) {
+        throw new ApiError(404, "channel does not exits");
+    }
+
+    return res
+        .status(200)
+        .json(
+            ApiResponse(200, channel[0], "User channel fetched successfully")
+        );
+});
 export {
     registerUser,
     loginUser,
@@ -332,4 +404,5 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
+    getUserChannelProfile,
 };
